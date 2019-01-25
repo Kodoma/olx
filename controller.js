081fd82be083f7cf8bd0600832eaa1b9f9485704
German Caceres <german.caceres@mercadolibre.com>
2019-01-24 22:40:33 -0300

    clean code

--- controller.js
+++ controller.js
@@ -12,6 +12,6 @@ module.exports = http.createServer((req, res) => {
             req.method + ' Endpoint: ' +
             reqUrl.pathname);
 
-        return service.sampleRequest(req, res);
+        service.sampleRequest(req, res);
     } 
 });
--- service.js
+++ service.js
@@ -2,9 +2,6 @@ var https = require('https');
 
 exports.sampleRequest = function(req, res) {
   return https.get('https://api.mercadolibre.com/sites/MLA/search?q=ipod', (response) => {
-    console.log('statusCode:', response.statusCode);
-    console.log('headers:', response.headers);
-  
     let data = '';
 
     // A chunk of data has been recieved.
@@ -12,10 +9,8 @@ exports.sampleRequest = function(req, res) {
       data += chunk;
     });
 
-    // The whole response has been received. Print out the result.
     response.on('end', () => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(data));
     });
 
