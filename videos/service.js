4d81ac035f0be230689ae3f40d041f15779666c2
German Caceres <german.caceres@mercadolibre.com>
2019-01-28 09:38:46 -0300

    add final exercise

--- videos/service.js
+++ videos/service.js
@@ -1,20 +1,23 @@
 var https = require('https');
 
-exports.sampleRequest = function(req, res) {
-  return https.get('https://api.mercadolibre.com/sites/MLA/search?q=ipod', (response) => {
-    let data = '';
-
-    // A chunk of data has been recieved.
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(data));
-    });
-
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
+exports.sampleRequest = function (req, res) {
+  const pageNumber = 1;
+  https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${pageNumber}`, response => {
+    response.on('data', movies => {
+      const result = JSON.parse(movies)
+      const data = result.data
+      const parallelPromises = []
+      for (let i = 2; i <= result.total_pages; i++) {
+        https.get(`https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${i}`, res => {
+          res.on('data', movie => {
+            const aux = JSON.parse(movie)
+            aux.data.forEach(currValue => data.push(currValue))
+            data.sort((a, b) => a.Year - b.Year);
+            const reversed = data.reverse().slice(0, 10)
+            reversed.forEach(currTitle => console.log(currTitle.Title))
+          })
+        })
+      }
+    })
+  }).on('error', error => console.log(error))
 };
